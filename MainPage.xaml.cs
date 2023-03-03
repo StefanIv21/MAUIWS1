5afe995031dcb1e24c8b5897d0addd50a580e259
Ionescu Valentin Stefan <94042909+StefanIv21@users.noreply.github.com>
2023-03-03 20:41:26 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiWS1;
+
+public partial class MainPage : ContentPage
+{
+	String s;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    private void TextChanged(object sender, TextChangedEventArgs e)
+    {
+        s = e.NewTextValue;
+    }
+
+	private void convertmoney(object sender, EventArgs e)
+	{
+		double sum = 0;
+      
+        try
+		{
+			sum = Convert.ToDouble(s) * 4.95;
+            double sum2= Convert.ToDouble(sum.ToString("0.00"));
+            display.Text = $"{sum2} Ron";
+   
+
+        }
+		catch (FormatException)
+		{
+			display.Text = "Invalid Format";
+		}
+	}
+
+        private void clicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync("NewPage");
+        }
+    
+}
+
